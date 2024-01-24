c018f44fba841576c2709e31840ad2dd33b854a8
dsthiago <thiagosd.2.18@gmail.com>
2024-01-23 21:00:37 -0300

    Add react-router-dom and react-scroll dependencies
    Update social media image and touch icon URLs
    
    Refactor services section CSS for responsiveness
    
    Add Firebase analytics and navigation logging to Hero component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,51 +1,56 @@
-import React from 'react';
-import '../Services.css'; // Ensure to create this CSS file
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import '../Services.css'; 
 
 import image_0 from '../assets/images/services_tree_00_white.png';
 import image_1 from '../assets/images/services_tree_01_white.png';
 
 function ServicesSection() {
+    useEffect(() => {
+        AOS.init();
+    }, []);
     return (
         <div className="services-container" id="services" data-aos="fade-up">
             {/* <h1>Sobre</h1> */}
             <div className="services-section">
-                <div className="image-container">
-                    <img src={image_0} alt="About Us" />
+                <div className="service">
+                    <div className="image-container">
+                        <img src={image_0} alt="About Us" />
+                    </div>
+                    <div className="text-container">
+                        <p><b>A Semente</b> &mdash; Como sua advogada familiarista, ofereço <span className="highlight-on-scroll" data-aos="highlight">acompanhamento especializado em contratos de técnica de reprodução humana assistida</span>, garantindo que seus direitos e expectativas sejam plenamente respeitados neste momento tão significativo. Também me dedico a elaborar <span className="highlight-on-scroll" data-aos="highlight">planos de parto</span> personalizados, assegurando que suas preferências sejam legalmente reconhecidas. No campo da paternidade, conduzo <span className="highlight-on-scroll" data-aos="highlight">investigações de paternidade</span> com sensibilidade e precisão, buscando esclarecer e estabelecer vínculos familiares importantes. Além disso, ofereço suporte em casos de <span className="highlight-on-scroll" data-aos="highlight">alimentos gravídicos</span>, focando na proteção e no bem-estar da mãe e do bebê durante a gravidez.
+                        </p>
+                    </div>
                 </div>
-                <div className="text-container">
-                    <p>
-                    Acompanhamento em contratos de Técnica de Reprodução Humana Assistida, plano de parto, investigação de paternidade, alimentos gravídicos.
-                    </p>
+                <div className="service">
+                    <div className="image-container">
+                        <img src={image_1} alt="About Us" />
+                    </div>
+                    <div className="text-container">
+                        <p><b>Ramos em Expansão</b> &mdash; Ofereço serviços que abrangem desde a criação de <span className="highlight-on-scroll" data-aos="highlight">contratos de namoro</span>, preservando independências enquanto celebra o relacionamento, até a elaboração de <span className="highlight-on-scroll" data-aos="highlight">pactos antenupciais</span>, assegurando acordos personalizados para futuros cônjuges. Também elaboro <span className="highlight-on-scroll" data-aos="highlight">contratos de união estável</span>, garantindo reconhecimento e proteção legal. Em momentos de mudança, estruturo e conduzo a <span className="highlight-on-scroll" data-aos="highlight">modificação de regime de bens</span> e, em casos de separação, conduzo processos de <span className="highlight-on-scroll" data-aos="highlight">divórcio</span> e <span className="highlight-on-scroll" data-aos="highlight">partilha de bens</span> com sensibilidade e foco na equidade.
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div className="services-section">
-                <div className="image-container">
-                    <img src={image_1} alt="About Us" />
+                <div className="service">
+                    <div className="image-container">
+                        <img src={image_1} alt="About Us" />
+                    </div>
+                    <div className="text-container">
+                        <p>
+                        Contrato de namoro; Pacto antenupcial; contrato de união estável; modificação de regime de bens, divórcio e partilha de bens.
+                        </p>
+                    </div>
                 </div>
-                <div className="text-container">
-                    <p>
-                    Contrato de namoro; Pacto antenupcial; contrato de união estável; modificação de regime de bens, divórcio e partilha de bens.
-                    </p>
-                </div>
-            </div>
-            <div className="services-section">
-                <div className="image-container">
-                    <img src={image_1} alt="About Us" />
-                </div>
-                <div className="text-container">
-                    <p>
-                    Regulamentação de guarda, lar referência e regime de convivência, alienação parental, fixação e revisão de pensão, alimentos avoengos, adoção, reconhecimento de paternidade/maternidade socioafetiva. 
-                    </p>
-                </div>
-            </div>
-            <div className="services-section">
-                <div className="image-container">
-                    <img src={image_1} alt="About Us" />
-                </div>
-                <div className="text-container">
-                    <p>
-                    Planejamento sucessório, auxílio e elaboração de testamentos, doação, elaboração de diretivas de ultima vontade, curatela, inventário, fraude patrimonial.
-                    </p>
+                <div className="service">
+                    <div className="image-container">
+                        <img src={image_1} alt="About Us" />
+                    </div>
+                    <div className="text-container">
+                        <p>
+                        Contrato de namoro; Pacto antenupcial; contrato de união estável; modificação de regime de bens, divórcio e partilha de bens.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
