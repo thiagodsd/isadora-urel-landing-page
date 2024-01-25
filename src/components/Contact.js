63f135a2da4b08642c29feaf690b4f33a6efe4d8
dsthiago <thiagosd.2.18@gmail.com>
2024-01-25 19:48:06 -0300

    Update styles in About, Footer, Services, App, and Contact components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,51 +1,78 @@
 import React from 'react';
 import '../Contact.css'; // Ensure to create this CSS file
 
+import contact_00_img from '../assets/images/contact_00_white.png';
+import contact_01_img from '../assets/images/contact_01_white.png';
+import contact_02_img from '../assets/images/contact_02_white.png';
+import contact_03_img from '../assets/images/contact_03_white.png';
+
 function ContactSection() {
     return (
         <div className="contact-container" id="contact" data-aos="fade-up">
             {/* <h1>Serviços</h1> */}
         <div className="contact-section">
             <div className="contact">
-                <h3>Atendimento</h3>
-                <p>
-                Navegar por questões jurídicas pode ser um processo complicado e gerar tensão. Compreendemos que este é um período delicado, e encontrar um profissional que inspire confiança e ofereça acolhimento representa um desafio significativo, especialmente em casos de direito de família e sucessões.
-                </p>
-                <br/>
-                <p>
-                Com o objetivo de proporcionar o suporte essencial neste momento crucial, reservamos um horário especial em nossa agenda para atendê-lo(a) online. Assim, você terá a oportunidade de expor suas necessidades e preocupações e conhecer nossa especialista, que está pronta para orientá-lo(a) com empatia e expertise.
-                </p>
+                <div className="contact-image">
+                    <img src={contact_00_img} alt="About Us" />
+                </div>
+                <div className="contact-text">
+                    <h2>Atendimento</h2>
+                    <p>
+                        Navegar por questões jurídicas pode ser um processo complicado e gerar tensão. Compreendemos que este é um período delicado, e encontrar um profissional que inspire confiança e ofereça acolhimento representa um desafio significativo, especialmente em casos de direito de família e sucessões.
+                    </p>
+                    <br/>
+                    <p>
+                        Com o objetivo de proporcionar o suporte essencial neste momento crucial, reservamos um horário especial em nossa agenda para atendê-lo(a) online. Assim, você terá a oportunidade de expor suas necessidades e preocupações e conhecer nossa especialista, que está pronta para orientá-lo(a) com empatia e expertise.
+                    </p>
+                </div>
             </div>
             <div className="contact">
-                <h3>Consulta</h3>
-                <p>
-                    Enfrentar desafios legais pode ser complexo e estressante. Nossa consultoria é especialmente projetada para pessoas que estão enfrentando questões delicadas de direito de família e sucessões e precisam de orientações claras sobre como proceder. Aqui, você encontrará o auxílio necessário para compreender suas opções e planejar seus próximos passos com confiança. Nós fornecemos o suporte necessário para que você tome decisões informadas.
-                </p>
-                <br/>
-                <p>
-                    Nosso atendimento é personalizado e realizado online, sempre com horários marcados, pois entendemos que cada situação é única. Agende sua consulta em um momento que seja conveniente para você e esclareça suas dúvidas jurídicas de forma personalizada, no conforto de sua casa.
-                </p>
-                <br/>
-                <p>
-                    Caso seja necessário, realizamos uma análise detalhada de todos os documentos relevantes para o seu caso, garantindo que nenhuma informação importante seja perdida e que sua estratégia legal seja embasada em uma compreensão completa da situação.
-                </p>
+                <div className="contact-image">
+                    <img src={contact_01_img} alt="About Us" />
+                </div>
+                <div className="contact-text">
+                    <h2>Consulta</h2>
+                    <p>
+                        Navegar por questões jurídicas pode ser um processo complicado e gerar tensão. Compreendemos que este é um período delicado, e encontrar um profissional que inspire confiança e ofereça acolhimento representa um desafio significativo, especialmente em casos de direito de família e sucessões.
+                    </p>
+                    <br/>
+                    <p>
+                        Com o objetivo de proporcionar o suporte essencial neste momento crucial, reservamos um horário especial em nossa agenda para atendê-lo(a) online. Assim, você terá a oportunidade de expor suas necessidades e preocupações e conhecer nossa especialista, que está pronta para orientá-lo(a) com empatia e expertise.
+                    </p>
+                </div>
             </div>
             <div className="contact">
-                <h3>Mentoria</h3>
-                <p>
-                    Nossa mentoria é especialmente voltada para profissionais do direito que buscam um caminho sólido nas áreas de direito de família, sucessões e planejamento patrimonial.
-                </p>
-                <br/>
-                <p>
-                    Este serviço é ideal tanto para o aprimoramento em casos concretos, visando otimizar o atendimento aos clientes finais, quanto para impulsionar sua a carreira, destacando e desenvolvendo as habilidades para tornar sua prática jurídica mais lucrativa e eficiente.
-                </p>
-                <br/>
-                <p>
-                    Nossa mentora, com seu perfil excepcional, é uma referência no campo jurídico. Atuando como professora, Phd, advogada e palestrante, ela tem uma experiência diversificada, englobando tanto a advocacia quanto a docência em projetos complexos. Seu profundo conhecimento é crucial para auxiliar você a se tornar um profissional ainda mais competente e bem-sucedido no dinâmico mundo do direito.
-                </p>
+                <div className="contact-image">
+                    <img src={contact_02_img} alt="About Us" />
+                </div>
+                <div className="contact-text">
+                    <h2>Parceria</h2>
+                    <p>
+                        Navegar por questões jurídicas pode ser um processo complicado e gerar tensão. Compreendemos que este é um período delicado, e encontrar um profissional que inspire confiança e ofereça acolhimento representa um desafio significativo, especialmente em casos de direito de família e sucessões.
+                    </p>
+                    <br/>
+                    <p>
+                        Com o objetivo de proporcionar o suporte essencial neste momento crucial, reservamos um horário especial em nossa agenda para atendê-lo(a) online. Assim, você terá a oportunidade de expor suas necessidades e preocupações e conhecer nossa especialista, que está pronta para orientá-lo(a) com empatia e expertise.
+                    </p>
+                </div>
             </div>
+            <div className="contact">
+                <div className="contact-image">
+                    <img src={contact_03_img} alt="About Us" />
+                </div>
+                <div className="contact-text">
+                    <h2>Mentoria</h2>
+                    <p>
+                        Navegar por questões jurídicas pode ser um processo complicado e gerar tensão. Compreendemos que este é um período delicado, e encontrar um profissional que inspire confiança e ofereça acolhimento representa um desafio significativo, especialmente em casos de direito de família e sucessões.
+                    </p>
+                    <br/>
+                    <p>
+                        Com o objetivo de proporcionar o suporte essencial neste momento crucial, reservamos um horário especial em nossa agenda para atendê-lo(a) online. Assim, você terá a oportunidade de expor suas necessidades e preocupações e conhecer nossa especialista, que está pronta para orientá-lo(a) com empatia e expertise.
+                    </p>
+                </div>
             </div>
         </div>
+    </div>
     );
 }
 
--- src/components/Services.js
+++ src/components/Services.js
@@ -19,46 +19,50 @@ function ServicesSection() {
                 <div className="service">
                     <div className="image-container">
                         <img src={image_0} alt="About Us" />
+                        <p><b>Início da Vida Familiar</b> &mdash; O nascimento de uma criança sempre marca o início de uma nova fase na família.</p>
                     </div>
                     <div className="text-container">
-                        <p><b>Início da Vida Familiar</b> &mdash; O nascimento de uma criança sempre marca o início de uma nova fase na família.</p>
-                        <br/>
-                        <p>Como sua advogada familiarista, ofereço <span className="highlight-on-scroll" data-aos="highlight">acompanhamento especializado em contratos de técnica de reprodução humana assistida</span>, garantindo que seus direitos e expectativas sejam plenamente respeitados neste momento tão significativo. Também me dedico a elaborar <span className="highlight-on-scroll" data-aos="highlight">planos de parto</span> personalizados, assegurando que suas preferências sejam legalmente reconhecidas. No campo da paternidade, conduzo <span className="highlight-on-scroll" data-aos="highlight">investigações de paternidade</span> com sensibilidade e precisão, buscando esclarecer e estabelecer vínculos familiares importantes. Além disso, ofereço suporte em casos de <span className="highlight-on-scroll" data-aos="highlight">alimentos gravídicos</span>, focando na proteção e no bem-estar da mãe e do bebê durante a gravidez.
+                        {/* <p>Como sua advogada familiarista, ofereço <span className="highlight-on-scroll" data-aos="highlight">acompanhamento especializado em contratos de técnica de reprodução humana assistida</span>, garantindo que seus direitos e expectativas sejam plenamente respeitados neste momento tão significativo. Também me dedico a elaborar <span className="highlight-on-scroll" data-aos="highlight">planos de parto</span> personalizados, assegurando que suas preferências sejam legalmente reconhecidas. No campo da paternidade, conduzo <span className="highlight-on-scroll" data-aos="highlight">investigações de paternidade</span> com sensibilidade e precisão, buscando esclarecer e estabelecer vínculos familiares importantes. Além disso, ofereço suporte em casos de <span className="highlight-on-scroll" data-aos="highlight">alimentos gravídicos</span>, focando na proteção e no bem-estar da mãe e do bebê durante a gravidez.
+                        </p> */}
+                        <p>
+                            <span className="highlight-on-scroll" data-aos="highlight">contratos de técnica de reprodução humana assistida</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">planos de parto</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">investigações de paternidade</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">alimentos gravídicos</span>
                         </p>
                     </div>
                 </div>
                 <div className="service">
                     <div className="image-container">
                         <img src={image_1} alt="About Us" />
+                        <p><b>Diversidade e Crescimento</b> &mdash; Com o passar do tempo surgem novas ramificações.</p>
                     </div>
                     <div className="text-container">
-                        <p><b>Diversidade e Crescimento</b> &mdash; Com o passar do tempo surgem novas ramificações.</p>
-                        <br/>
-                        <p>De forma análoga, famílias se expandem e diversificam. Nessa fase meus serviços abrangem desde a criação de <span className="highlight-on-scroll" data-aos="highlight">contratos de namoro</span>, preservando independências enquanto celebra o relacionamento, até a elaboração de <span className="highlight-on-scroll" data-aos="highlight">pactos antenupciais</span>, assegurando acordos personalizados para futuros cônjuges. Também elaboro <span className="highlight-on-scroll" data-aos="highlight">contratos de união estável</span>, garantindo reconhecimento e proteção legal. Em momentos de mudança, estruturo e conduzo a <span className="highlight-on-scroll" data-aos="highlight">modificação de regime de bens</span> e, em casos de separação, conduzo processos de <span className="highlight-on-scroll" data-aos="highlight">divórcio</span> e <span className="highlight-on-scroll" data-aos="highlight">partilha de bens</span> com sensibilidade e foco na equidade.
+                        {/* <p>De forma análoga, famílias se expandem e diversificam. Nessa fase meus serviços abrangem desde a criação de <span className="highlight-on-scroll" data-aos="highlight">contratos de namoro</span>, preservando independências enquanto celebra o relacionamento, até a elaboração de <span className="highlight-on-scroll" data-aos="highlight">pactos antenupciais</span>, assegurando acordos personalizados para futuros cônjuges. Também elaboro <span className="highlight-on-scroll" data-aos="highlight">contratos de união estável</span>, garantindo reconhecimento e proteção legal. Em momentos de mudança, estruturo e conduzo a <span className="highlight-on-scroll" data-aos="highlight">modificação de regime de bens</span> e, em casos de separação, conduzo processos de <span className="highlight-on-scroll" data-aos="highlight">divórcio</span> e <span className="highlight-on-scroll" data-aos="highlight">partilha de bens</span> com sensibilidade e foco na equidade.
+                        </p> */}
+                        <p><span className="highlight-on-scroll" data-aos="highlight">contratos de namoro</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">pactos antenupciais</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">contratos de união estável</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">modificação de regime de bens</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">divórcio</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">partilha de bens</span>
                         </p>
                     </div>
                 </div>
                 <div className="service">
                     <div className="image-container">
                         <img src={image_2} alt="About Us" />
+                        <p><b>Estabelecendo Bases Sólidas</b> &mdash; Enraizar é necessário para garantir estabilidade e assegurar o crescimento saudável.</p>
                     </div>
                     <div className="text-container">
-                        <p><b>Estabelecendo Bases Sólidas</b> &mdash; Enraizar é necessário para garantir estabilidade e assegurar o crescimento saudável.</p>
-                        <br/>
-                        <p>
+                        {/* <p>
                         Meu papel na sua solidificação familiar é através assistência na <span className="highlight-on-scroll" data-aos="highlight">regulamentação de guarda</span>, focando em um ambiente estável e harmonioso para a criança. Previno e combato situações de <span className="highlight-on-scroll" data-aos="highlight">alienação parental</span>, protegendo os direitos e o bem-estar dos menores. Trabalho com <span className="highlight-on-scroll" data-aos="highlight">fixação e revisão de pensão alimentícia</span>, buscando justiça e equidade financeira. Em casos de <span className="highlight-on-scroll" data-aos="highlight">alimentos avoengos</span>, garanto o suporte essencial de toda a família. Auxilio famílias no processo de <span className="highlight-on-scroll" data-aos="highlight">adoção</span> e atuo no <span className="highlight-on-scroll" data-aos="highlight">reconhecimento de paternidade &amp; maternidade socioafetiva</span>, fortalecendo laços familiares.
+                        </p> */}
+                        <p>
+                        <span className="highlight-on-scroll" data-aos="highlight">regulamentação de guarda</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">alienação parental</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">fixação e revisão de pensão alimentícia</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">alimentos avoengos</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">adoção</span> &#183; <span className="highlight-on-scroll" data-aos="highlight">reconhecimento de paternidade &amp; maternidade socioafetiva</span>
                         </p>
                     </div>
                 </div>
                 <div className="service">
                     <div className="image-container">
                         <img src={image_3} alt="About Us" />
+                        <p><b>Legado</b> &mdash; Com a maturidade vem os frutos e o fortalecimento de novos ciclos.</p>
                     </div>
                     <div className="text-container">
-                        <p><b>Legado</b> &mdash; Com a maturidade vem os frutos e o fortalecimento de novos ciclos.</p>
-                        <br/>
                         <p>
-                        Nessa etapa sou sua aliada no <span className="highlight-on-scroll" data-aos="highlight">planejamento sucessório</span>, guiando a preparação de um futuro seguro para sua família. Me dedico à <span className="highlight-on-scroll" data-aos="highlight">elaboração de testamentos e doações</span>, assegurando que seus desejos sejam claramente expressos e legalmente atendidos, fornecendo orientação em <span className="highlight-on-scroll" data-aos="highlight">diretivas de última vontade</span>, me comprometendo a respeitar e implementar suas decisões finais, além de atuar na prevenção e combate de <span className="highlight-on-scroll" data-aos="highlight">fraude patrimonial</span>. Na área de <span className="highlight-on-scroll" data-aos="highlight">curatela</span>, atuo para proteger os interesses daqueles que não podem fazê-lo por si mesmos. Gerencio processos de <span className="highlight-on-scroll" data-aos="highlight">inventário</span>, simplificando a transferência de bens após o falecimento.
+                        <span className="highlight-on-scroll" data-aos="highlight">planejamento sucessório</span> &#183;  <span className="highlight-on-scroll" data-aos="highlight">elaboração de testamentos e doações</span> &#183;  <span className="highlight-on-scroll" data-aos="highlight">diretivas de última vontade</span> &#183;  <span className="highlight-on-scroll" data-aos="highlight">fraude patrimonial</span> &#183;  <span className="highlight-on-scroll" data-aos="highlight">curatela</span> &#183;  <span className="highlight-on-scroll" data-aos="highlight">inventário</span>
                         </p>
                     </div>
                 </div>
