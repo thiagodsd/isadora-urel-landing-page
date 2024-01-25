264203d91ab864040e1bb9de56e6e0b5d9e25769
dsthiago <thiagosd.2.18@gmail.com>
2024-01-25 07:51:28 -0300

    Update position of hero-text in App.css

--- src/components/Services.js
+++ src/components/Services.js
@@ -19,7 +19,9 @@ function ServicesSection() {
                         <img src={image_0} alt="About Us" />
                     </div>
                     <div className="text-container">
-                        <p><b>A Semente</b> &mdash; Como sua advogada familiarista, ofereço <span className="highlight-on-scroll" data-aos="highlight">acompanhamento especializado em contratos de técnica de reprodução humana assistida</span>, garantindo que seus direitos e expectativas sejam plenamente respeitados neste momento tão significativo. Também me dedico a elaborar <span className="highlight-on-scroll" data-aos="highlight">planos de parto</span> personalizados, assegurando que suas preferências sejam legalmente reconhecidas. No campo da paternidade, conduzo <span className="highlight-on-scroll" data-aos="highlight">investigações de paternidade</span> com sensibilidade e precisão, buscando esclarecer e estabelecer vínculos familiares importantes. Além disso, ofereço suporte em casos de <span className="highlight-on-scroll" data-aos="highlight">alimentos gravídicos</span>, focando na proteção e no bem-estar da mãe e do bebê durante a gravidez.
+                        <p><b>Início da Vida Familiar</b> &mdash; O nascimento de uma criança sempre marca o início de uma nova fase na família.</p>
+                        <br/>
+                        <p>Como sua advogada familiarista, ofereço <span className="highlight-on-scroll" data-aos="highlight">acompanhamento especializado em contratos de técnica de reprodução humana assistida</span>, garantindo que seus direitos e expectativas sejam plenamente respeitados neste momento tão significativo. Também me dedico a elaborar <span className="highlight-on-scroll" data-aos="highlight">planos de parto</span> personalizados, assegurando que suas preferências sejam legalmente reconhecidas. No campo da paternidade, conduzo <span className="highlight-on-scroll" data-aos="highlight">investigações de paternidade</span> com sensibilidade e precisão, buscando esclarecer e estabelecer vínculos familiares importantes. Além disso, ofereço suporte em casos de <span className="highlight-on-scroll" data-aos="highlight">alimentos gravídicos</span>, focando na proteção e no bem-estar da mãe e do bebê durante a gravidez.
                         </p>
                     </div>
                 </div>
@@ -28,7 +30,9 @@ function ServicesSection() {
                         <img src={image_1} alt="About Us" />
                     </div>
                     <div className="text-container">
-                        <p><b>Ramos em Expansão</b> &mdash; Ofereço serviços que abrangem desde a criação de <span className="highlight-on-scroll" data-aos="highlight">contratos de namoro</span>, preservando independências enquanto celebra o relacionamento, até a elaboração de <span className="highlight-on-scroll" data-aos="highlight">pactos antenupciais</span>, assegurando acordos personalizados para futuros cônjuges. Também elaboro <span className="highlight-on-scroll" data-aos="highlight">contratos de união estável</span>, garantindo reconhecimento e proteção legal. Em momentos de mudança, estruturo e conduzo a <span className="highlight-on-scroll" data-aos="highlight">modificação de regime de bens</span> e, em casos de separação, conduzo processos de <span className="highlight-on-scroll" data-aos="highlight">divórcio</span> e <span className="highlight-on-scroll" data-aos="highlight">partilha de bens</span> com sensibilidade e foco na equidade.
+                        <p><b>Diversidade e Crescimento</b> &mdash; Com o passar do tempo surgem novas ramificações.</p>
+                        <br/>
+                        <p>De forma análoga, famílias se expandem e diversificam. Nessa fase meus serviços abrangem desde a criação de <span className="highlight-on-scroll" data-aos="highlight">contratos de namoro</span>, preservando independências enquanto celebra o relacionamento, até a elaboração de <span className="highlight-on-scroll" data-aos="highlight">pactos antenupciais</span>, assegurando acordos personalizados para futuros cônjuges. Também elaboro <span className="highlight-on-scroll" data-aos="highlight">contratos de união estável</span>, garantindo reconhecimento e proteção legal. Em momentos de mudança, estruturo e conduzo a <span className="highlight-on-scroll" data-aos="highlight">modificação de regime de bens</span> e, em casos de separação, conduzo processos de <span className="highlight-on-scroll" data-aos="highlight">divórcio</span> e <span className="highlight-on-scroll" data-aos="highlight">partilha de bens</span> com sensibilidade e foco na equidade.
                         </p>
                     </div>
                 </div>
@@ -37,8 +41,10 @@ function ServicesSection() {
                         <img src={image_1} alt="About Us" />
                     </div>
                     <div className="text-container">
+                        <p><b>Estabelecendo Bases Sólidas</b> &mdash; Enraizar é necessário para garantir estabilidade e assegurar o crescimento saudável.</p>
+                        <br/>
                         <p>
-                        Contrato de namoro; Pacto antenupcial; contrato de união estável; modificação de regime de bens, divórcio e partilha de bens.
+                        Meu papel na sua solidificação familiar é através assistência na <span className="highlight-on-scroll" data-aos="highlight">regulamentação de guarda</span>, focando em um ambiente estável e harmonioso para a criança. Previno e combato situações de <span className="highlight-on-scroll" data-aos="highlight">alienação parental</span>, protegendo os direitos e o bem-estar dos menores. Trabalho com <span className="highlight-on-scroll" data-aos="highlight">fixação e revisão de pensão alimentícia</span>, buscando justiça e equidade financeira. Em casos de <span className="highlight-on-scroll" data-aos="highlight">alimentos avoengos</span>, garanto o suporte essencial de toda a família. Auxilio famílias no processo de <span className="highlight-on-scroll" data-aos="highlight">adoção</span> e atuo no <span className="highlight-on-scroll" data-aos="highlight">reconhecimento de paternidade &amp; maternidade socioafetiva</span>, fortalecendo laços familiares.
                         </p>
                     </div>
                 </div>
@@ -47,8 +53,10 @@ function ServicesSection() {
                         <img src={image_1} alt="About Us" />
                     </div>
                     <div className="text-container">
+                        <p><b>Legado</b> &mdash; Com a maturidade vem os frutos e o fortalecimento de novos ciclos.</p>
+                        <br/>
                         <p>
-                        Contrato de namoro; Pacto antenupcial; contrato de união estável; modificação de regime de bens, divórcio e partilha de bens.
+                        Nessa etapa sou sua aliada no <span className="highlight-on-scroll" data-aos="highlight">planejamento sucessório</span>, guiando a preparação de um futuro seguro para sua família. Me dedico à <span className="highlight-on-scroll" data-aos="highlight">elaboração de testamentos e doações</span>, assegurando que seus desejos sejam claramente expressos e legalmente atendidos, fornecendo orientação em <span className="highlight-on-scroll" data-aos="highlight">diretivas de última vontade</span>, me comprometendo a respeitar e implementar suas decisões finais, além de atuar na prevenção e combate de <span className="highlight-on-scroll" data-aos="highlight">fraude patrimonial</span>. Na área de <span className="highlight-on-scroll" data-aos="highlight">curatela</span>, atuo para proteger os interesses daqueles que não podem fazê-lo por si mesmos. Gerencio processos de <span className="highlight-on-scroll" data-aos="highlight">inventário</span>, simplificando a transferência de bens após o falecimento.
                         </p>
                     </div>
                 </div>
