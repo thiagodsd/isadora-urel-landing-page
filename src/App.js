fa19e51de503003173c4d3725f2344ae388bdae7
dsthiago <thiagosd.2.18@gmail.com>
2024-01-26 00:05:05 -0300

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
 import Contact from './components/Contact';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 import WhatsApp from './components/WhatsApp';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -34,7 +34,7 @@ function App() {
             <Contact/>
             <WhatsApp/>
         </main>
-        {/* <Footer /> */}
+        <Footer />
         <div className="back-to-top-back">
             <div className="back-to-top" onClick={scrollToTop} aria-label="Back to top"></div>
         </div>
