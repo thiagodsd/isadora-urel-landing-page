53bc2cd0e4696e97deef14734f915cca967429b1
dsthiago <thiagosd.2.18@gmail.com>
2024-01-26 00:05:05 -0300

    Add Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import '../Footer.css';
 
 const Footer = () => {
     return (
         <footer>
-            <p style={{textAlign: 'center', color: '#fff', backgroundColor: '#000', padding: '1rem'}}>
-                2024
-            </p>
+            <div className="footer-container" id="contact">
+            <div className="footer-section">
+                <div className="footer-text">
+                    <p>Site desenvolvido por</p>
+                    <a href="https://dszero.com.br/" target="_blank" rel="noreferrer">
+                    &#948;S
+                    </a>
+                </div>
+                {/* <div className="footer-text">
+                    <p>Empoderando com Ciência de Dados</p>
+                </div> */}
+            </div>
+        </div>  
         </footer>
     );
 }
